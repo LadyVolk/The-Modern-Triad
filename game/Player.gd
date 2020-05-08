2fa4008ed6d24e252dbb4618cf356bd6ba979d25
Pedro Arrochela Lobo <40302551+LadyVolk@users.noreply.github.com>
2020-05-08 20:01:03 +0100

    player rotates according to mouse

--- game/Player.gd
+++ game/Player.gd
@@ -4,6 +4,7 @@ export var speed: int
 
 func _ready():
 	$Label.text = "life is great"
+	$TextureRect.rect_pivot_offset = $TextureRect.rect_size/2
 
 
 func _process(delta):
@@ -15,7 +16,14 @@ func _process(delta):
 	if Input.is_action_pressed("player_right"):
 		mov_vector.x += 1
 	if Input.is_action_pressed("player_left"):
-		mov_vector.x -= 1
+		mov_vector.x -= 1 
 		
 	mov_vector = mov_vector.normalized()
 	position += mov_vector * speed * delta
+	
+	var mouse_pos = get_local_mouse_position()
+	var center_player = $TextureRect.rect_size/2
+	
+	var direction = mouse_pos - center_player
+	
+	$TextureRect.rect_rotation = rad2deg(direction.angle())+90
