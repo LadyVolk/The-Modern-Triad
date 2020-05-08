7d8497b13f92111911afcac9157b240e10d1403e
Pedro Arrochela Lobo <40302551+LadyVolk@users.noreply.github.com>
2020-05-08 20:20:21 +0100

    started player dash

--- game/Player.gd
+++ game/Player.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 export var speed: int
 
+export var dash_strength = 200
+
 func _ready():
 	$Label.text = "life is great"
 	$TextureRect.rect_pivot_offset = $TextureRect.rect_size/2
+	
 
 
 func _process(delta):
@@ -17,13 +20,25 @@ func _process(delta):
 		mov_vector.x += 1
 	if Input.is_action_pressed("player_left"):
 		mov_vector.x -= 1 
-		
+	
 	mov_vector = mov_vector.normalized()
 	position += mov_vector * speed * delta
+	
+	
+	$TextureRect.rect_rotation = rad2deg(get_player_direction().angle())+90
+
+func _input(event):
+	if event.is_action_pressed("player_dash"):
+		dash()
+
+func get_player_direction():
 	
 	var mouse_pos = get_local_mouse_position()
 	var center_player = $TextureRect.rect_size/2
 	
-	var direction = mouse_pos - center_player
+	return  (mouse_pos - center_player).normalized()
 	
-	$TextureRect.rect_rotation = rad2deg(direction.angle())+90
+func dash():
+	var direction = get_player_direction()
+	
+	position += direction * dash_strength
