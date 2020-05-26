491e29fbd4627958c01ddeb48424f480a792efb3
Pedro Arrochela Lobo <40302551+LadyVolk@users.noreply.github.com>
2020-05-26 20:01:09 +0100

    speed boost area working

--- game/Bosses/Depression/SpeedBoost.gd
+++ game/Bosses/Depression/SpeedBoost.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var region
 
 func _ready():
 	pass 
--- game/Level.gd
+++ game/Level.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 const PROJECTILE = preload("res://Projectile.tscn")
-
 const SPEED_BOOST = preload("res://Bosses/Depression/SpeedBoost.tscn")
 
 onready var player = $Player
+onready var timer = $SpeedBoostTimer
+
+enum REGION {left, right}
+
 
 func _ready():
 	player.connect("shoot", self, "player_shoot")
@@ -19,20 +22,37 @@ func player_shoot(pos, direction):
 
 func _input(event):
 	if event is InputEventKey:
-		if event.pressed and event.scancode == KEY_0:
-			create_boost()
 		if event.pressed and event.scancode == KEY_SPACE:
 			player.take_damage(30)
 
 func create_boost():
-	var new_boost = SPEED_BOOST.instance()
-	$SpeedBoosts.add_child(new_boost)
+	
+	var new_boost
+	
+	
 	randomize() 
 	var which_area
-	if randf() >= 0.5:
-		which_area = $BoostSpawnAreaL
+	if $SpeedBoosts.get_child_count() == 0:
+		new_boost = SPEED_BOOST.instance()
+		$SpeedBoosts.add_child(new_boost)
+		if randf() >= 0.5:
+			which_area = $BoostSpawnAreaL
+			new_boost.region = REGION.left
+		else:
+			which_area = $BoostSpawnAreaR
+			new_boost.region = REGION.right
+			
+	elif $SpeedBoosts.get_child_count() == 1:
+		new_boost = SPEED_BOOST.instance()
+		$SpeedBoosts.add_child(new_boost)
+		if $SpeedBoosts.get_child(0).region == REGION.left:
+			which_area = $BoostSpawnAreaR
+			new_boost.region = REGION.right
+		else:
+			which_area = $BoostSpawnAreaL
+			new_boost.region = REGION.left
 	else:
-		which_area = $BoostSpawnAreaR
+		return
 	
 	var shape = which_area.get_node("CollisionShape").shape
 	
@@ -41,7 +61,5 @@ func create_boost():
 	new_boost.position.y = rand_range(which_area.position.y-shape.extents.y, 
 									which_area.position.y+shape.extents.y)
 	
-	
-	
-	
+
 	
