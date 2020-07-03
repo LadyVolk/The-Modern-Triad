2db587db5665600e98c0aa05f8fcb34fd6badf09
Pedro Arrochela Lobo <40302551+LadyVolk@users.noreply.github.com>
2020-07-03 18:41:31 +0100

    simple menu added

---
+++ game/Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _ready():
+	pass 
+
+
+
+func _on_Start_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://Level.tscn")
+
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
